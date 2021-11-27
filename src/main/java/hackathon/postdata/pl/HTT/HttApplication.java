9b9e9e23fb21f732bd55605072021ebad4a4995c
YumaMasterpiece <s22463@pjwstk.edu.com>
2021-11-27 09:54:36 +0100

    start

---
+++ src/main/java/hackathon/postdata/pl/HTT/HttApplication.java
@@ -0,0 +1,13 @@
+package hackathon.postdata.pl.HTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackathon/postdata/pl/HTT/HttApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.postdata.pl.HTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
