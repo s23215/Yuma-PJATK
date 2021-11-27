5ce85905cdd4e6b22a737fc550b7d735d9b684e2
YumaMasterpiece <s22463@pjwstk.edu.com>
2021-11-27 12:23:31 +0100

    newStart

--- lab06/src/main/java/jaz/lab06/demo/DemoApplication.java
+++ lab06/src/main/java/jaz/lab06/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package hackathon.postdata.pl.HTT;
+package jaz.lab06.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HttApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- lab06/src/test/java/jaz/lab06/demo/DemoApplicationTests.java
+++ lab06/src/test/java/jaz/lab06/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package hackathon.postdata.pl.HTT;
+package jaz.lab06.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HttApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
